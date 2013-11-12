af0da7d395c905de1fdbefb18f4442c82a560b27
Leslie T <leslie_tanengsy@yahoo.com>
2013-11-12 21:06:34 +1100

    update order total after adding detail

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,12 +1,18 @@
 class OrderDetail < ActiveRecord::Base
   attr_accessible :quantity, :unit, :product_id, :unit_price, :order_id
 
+  after_save :update_order_total
+
   validates :unit, :product_id, :order_id, presence: true
   validates :unit_price, numericality: true
   validates :quantity, numericality: { greater_than: 0 }
 
   belongs_to :order
   belongs_to :product
+
+ def update_order_total
+    order.update_totals
+  end
 
   def subtotal
     quantity * unit_price
